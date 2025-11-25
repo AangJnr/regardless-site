12423fc6a4c87487906d2ea32ce87869f15490de
ulquiorra <cion.aang@gmail.com>
2025-11-25 20:08:06 +0000

    Added app store urls

--- test/viewmodels/app_container_viewmodel_test.dart
+++ test/viewmodels/app_container_viewmodel_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:regardless_site/app/app.locator.dart';
 
-import '../helpers/test_helpers.dart';
-
 void main() {
   group('AppContainerViewModel Tests -', () {
-    setUp(() => registerServices());
+    //setUp(() => registerServices());
     tearDown(() => locator.reset());
   });
 }
